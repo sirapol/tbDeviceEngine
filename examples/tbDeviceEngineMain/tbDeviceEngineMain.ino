0de5f236d0e06660569009d553aab26aaf49db3f
sirapol <s.sirapol@hotmail.com>
2024-03-14 01:56:46 +0700

    fix

--- examples/tbDeviceEngineMain/tbDeviceEngineMain.ino
+++ examples/tbDeviceEngineMain/tbDeviceEngineMain.ino
@@ -4,21 +4,21 @@
 void setup()
 {
     Serial.begin(9600);
-    tbDeviceEngine::setHost("your-thingsboard-host.com");
-    tbDeviceEngine::setToken("your-device-access-token");
+    // tbDeviceEngine::setHost("your-thingsboard-host.com");
+    // tbDeviceEngine::setToken("your-device-access-token");
     // Optionally set port if different from the default (1883)
     // tbDeviceEngine::setPort(1883);
 
     // Initialize your ThingsBoard client using the configured values
-    ThingsBoard client;
-    if (client.connect())
-    {
-        Serial.println("Connected to ThingsBoard!");
-    }
-    else
-    {
-        Serial.println("Failed to connect to ThingsBoard!");
-    }
+    // ThingsBoard client;
+    // if (client.connect())
+    // {
+    //     Serial.println("Connected to ThingsBoard!");
+    // }
+    // else
+    // {
+    //     Serial.println("Failed to connect to ThingsBoard!");
+    // }
 }
 
 void loop()
